0db7701e76c9f5a873497fc90bf79612fea479ac
Cristobal Diaz <cdiazm@smu.cl>
2022-03-02 16:30:10 -0300

    Add peering 5

--- peering.tf
+++ peering.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network_peering" "peering-a" {
   name                         = "cl-peering-${var.app-name}-${count.index}"
   resource_group_name          = var.resource_group_name
   virtual_network_name         = var.address_spaces
-  remote_virtual_network_id    = [element(split(",", var.vnet_remote), count.index)]
+  remote_virtual_network_id    = element(split(",", var.vnet_remote), count.index)
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
