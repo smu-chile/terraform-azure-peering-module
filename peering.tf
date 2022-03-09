7826e7520ccc737114df533d2fe3fad88d0676ab
Cristobal Diaz <cdiazm@smu.cl>
2022-03-09 10:56:51 -0300

    peering fix 3

--- peering.tf
+++ peering.tf
@@ -2,18 +2,11 @@
 #### Peering Network ####
 
 resource "azurerm_virtual_network_peering" "peering-a" {
-  count                        = 2
+  count                        = length(split(",", var.vnet_remote))
   name                         = "cl-peering-${var.app-name}-${count.index}"
   resource_group_name          = var.resource_group_name
   virtual_network_name         = var.vnet_network
   remote_virtual_network_id    = element(split(",", var.vnet_remote), count.index)
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
-  provider = "azurerm.network"
 }
-
-
-
-
-
-
